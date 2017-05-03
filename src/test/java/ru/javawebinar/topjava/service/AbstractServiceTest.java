7778a34141f76febaef2585a6176e544e153cce0
Peter <mail@peter.by>
2017-05-03 11:59:39 +0300

    6 10 hibernate cache

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -10,12 +10,15 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.javawebinar.topjava.ActiveDbProfileResolver;
+import ru.javawebinar.topjava.Profiles;
 
 import java.util.concurrent.TimeUnit;
 
@@ -81,5 +84,16 @@ abstract public class AbstractServiceTest {
             result = cause;
         }
         return result;
+    }
+
+    @Autowired
+    private Environment environment;
+
+    public boolean checkOnJdbcProfile(){
+        for(String profile : environment.getActiveProfiles()){
+            if (profile.equals(Profiles.JDBC))
+                return true;
+        }
+        return false;
     }
 }
